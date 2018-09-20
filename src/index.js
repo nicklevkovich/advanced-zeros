85cd3a1eefc1e351dc061dacbf18dc872e5b9ff8
Nick Levkovich <nicklevkovic@gmail.com>
2018-09-20 21:22:35 +0300

    69/100 passing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ module.exports = function getZerosCount(number, base) {
 var simple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113],
     del = [],
     step = [],
-    sn = [],
     j = 0;
 for (i=0; i <= simple.length; i++)
 {
@@ -11,27 +10,26 @@ for (i=0; i <= simple.length; i++)
   {
     del.push(simple[i]);
     tmp/=simple[i];
-    step.push(0);
-    sn.push(0);
   };
-  while (base%simple[i]===0)
-  {
-    if (base%simple[i]===0){
-      base/=simple[i];
-      step[j]++;
-  }}; 
-  j++;
 };
+
 function maxPowerOf(p, n)
 {  
-var k = 0;
-var powerOfP = 1;
-while (true) 
-{
-  powerOfP *= p;
-  var addend = number / powerOfP;
-  if (addend == 0) return k;
-  k += addend;
-}
-}
+  var k = 0;
+  var powerOfP = 1;
+  while (true) 
+  {
+    powerOfP *= p;
+    var addend = Math.floor(number / powerOfP);
+    if (addend == 0) return k;
+    k += addend;
+  };
+};
+for (i=0; i<del.length; i++)
+  {
+    step.push(maxPowerOf(del[i],number));
+  }
+ step.sort(function(a,b){return a-b});
+ return(Math.floor(step[0]));
+
 }
